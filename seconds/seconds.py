94a976b2d7b2865eab43473221fecfc605dd8423
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-04-10 15:30:24 +0200

    seconds exercise. Exercise coded in a one line function.

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -1,7 +1,8 @@
-def seconds(change_this):
-    pass
     # Create a function that takes a list as a parameter,
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
+
+def seconds(number_list):
+    return (number_list[1::2])
 
 print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]

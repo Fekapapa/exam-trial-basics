ca5cb58a754400fe25b091dafb6788b18b9cd984
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-04-10 16:00:51 +0200

    countas exercise finally solced by import os path.

--- countas/count-as.py
+++ countas/count-as.py
@@ -1,11 +1,12 @@
     # Create a function that takes a filename as string parameter,
     # counts the occurances of the letter "a", and returns it as a number.
     # If the file does not exist, the function should return 0 and not break.
+from os import path
 
 def count_as(textfile):
     try:
-        #path = (r"C:\exam-trial-basics\countas\" + str(textfile))
-        my_file = open(r"C:\exam-trial-basics\countas\" + afile.txt, "r")
+        file_path = path.join("C:\exam-trial-basics\countas", textfile)
+        my_file = open(file_path, "r")
         lines = my_file.readline()
         a_number = 0
         for line in lines:
@@ -18,5 +19,5 @@ def count_as(textfile):
         return ("0")
 
 
-#print(count_as("afile.txt")) # should print 28
-#print(count_as("not-a-file")) # should print 0
+print(count_as("afile.txt")) # should print 28
+print(count_as("not-a-file")) # should print 0

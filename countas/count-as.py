60027e5e02474934eaa6ff952d209d29c188b0cf
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-04-10 14:42:40 +0200

    Countas exercise. Created the class and starting the counter function.

--- countas/count-as.py
+++ countas/count-as.py
@@ -1,8 +1,25 @@
-def count_as(change_this):
-    pass
     # Create a function that takes a filename as string parameter,
     # counts the occurances of the letter "a", and returns it as a number.
     # If the file does not exist, the function should return 0 and not break.
+
+def count_as(textfile):
+    def __init__(self, textfile=""):
+        self.textfile = textfile
+
+    def a_counter(self, textfile=""):
+        self.my_file = open(self.textfile, "r")
+        self.lines = self.my_file.readlines()
+        self.a_number = 0
+        for line in self.lines:
+            oneline = lines[linecount]
+            linecount += 1
+            halflinelength = len(oneline) // 2
+            charcount = 0
+            fulltext = ""
+            for char in range(halflinelenth):
+                fulltext += oneline[charcount]
+                charcount += 2
+
 
 print(count_as("afile.txt")) # should print 28
 print(count_as("not-a-file")) # should print 0
